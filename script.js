48d4670b82864c2c2bf97e387c79c948d8489591
Piyush kumar <piyushkumar841222@gmail>
2026-02-10 19:32:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Finding the length
+
+let numbers = [1, 2, 3, 4, 5]
+//   console.log(numbers[2]) // this will find the possion of the arrays
+//   console.log(numbers.length);// this will show the total length off arrays
+
+  // Changing their value
+   let name = ["apple", "banana", "orange", "papays"]
+   name["0"] = "lion" // this will change the arrays values using arrays location and aarrys name cause arrays are imutable
+   console.log(name);
+   
